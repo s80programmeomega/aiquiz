3594e1c35aee70bf1c3e57b00620618048e90210
loicernest444 <loicernest444@gmail.com>
2025-07-18 14:41:42 +0100

    Deactivation des fonctions de captcha et IA, modification des modeles Question et Asnwer, Ajout des types de reponses.

---
+++ app/Models/Answer.php
@@ -0,0 +1,40 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Answer extends Model
+{
+    use HasFactory;
+
+    protected $table = 'answers';
+
+    protected $fillable = [
+        'title',
+        'question_id',
+        'is_correct',
+        'type',
+    ];
+
+    protected $casts = [
+        'title'       => 'string',
+        'question_id' => 'integer',
+        'is_correct'  => 'boolean',
+    ];
+
+    public function question()
+    {
+        return $this->belongsTo(Question::class, 'question_id');
+    }
+
+    public function quizAnswers()
+    {
+        return $this->hasMany(QuestionAnswer::class, 'answer_id');
+    }
+
+    // public function getMediaUrlAttribute()
+    // {
+    //     return Storage::disk(config('app.media_disk'))->url($this->title);
+    // }
+}
